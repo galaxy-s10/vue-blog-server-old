d29c3c6bbeb673b5f91f7958ede13d019a583834
galaxy-s10 <2274751790@qq.com>
2020-02-22 15:40:08 +0800

    第一次修改comment关系

--- routers/comment_router.js
+++ routers/comment_router.js
@@ -1,21 +1,29 @@
 var express = require('express')
 var router = express.Router()
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op;
 var Comment = require('../models/Comment')
 var User = require('../models/User')
+var Article = require('../models/Article')
 
-//留言列表
+//留言板留言/单篇文章留言列表
 router.get('/', async function (req, res) {
     var { article_id } = req.query
     var { rows, count } = await Comment.findAndCountAll({
         where: { article_id },
         include: [{
             model: User,
-            attributes: ['username', 'role', 'avatar'],
-            as: 'from_user'
-        }],
+            attributes: ['username', 'avatar', 'role'],
+            as: "from_user",
+        },
+        {
+            attributes: ['username', 'avatar', 'role'],
+            model: User,
+            as: "to_user",
+        }
+        ],
         order: [['createdAt', 'DESC']],
     })
-    console.log('sssssssssssss')
     var lists = []
     // 遍历原本的数据
     for (var i = 0; i < rows.length; i++) {
@@ -37,6 +45,57 @@ router.get('/', async function (req, res) {
         }
     }
     res.json({ lists, count })
+})
+
+//所有文章留言列表
+router.get('/all', async function (req, res) {
+    var { rows, count } = await Comment.findAndCountAll({
+        where: {
+            article_id: { [Op.ne]: -1 }
+        },
+        // attributes: ['article_id','content','to_commentid'],
+        include: [
+            {
+                model: Article,
+                attributes: ['title'],
+            },
+            {
+                // model: Article,
+                // attributes: ['title'],
+                model: User,
+                attributes: ['username', 'avatar', 'role'],
+                as: "from_user",
+            },
+            {
+                attributes: ['username', 'avatar', 'role'],
+                model: User,
+                as: "to_user",
+            }
+        ],
+        order: [['article_id', 'ASC']],
+    })
+
+    var lists = []
+    // 遍历原本的数据
+    for (var i = 0; i < rows.length; i++) {
+        // var father = rows[i]
+        // 获取干净的JSON对象
+        var list = rows[i].get({
+            plain: true,
+        })
+        // 如果newrows里有to_commentid,则把这条数据插入它的父级
+        var child = []
+        for (var j = 0; j < rows.length; j++) {
+            if (rows[j].to_commentid == list.id) {
+                child.push(rows[j])
+            }
+        }
+        list.children = child
+        if (rows[i].to_commentid == -1) {
+            lists.push(list)
+        }
+    }
+    res.json(lists)
 })
 
 
