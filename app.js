e4e7e514942689b368d757cae2f27b110b658864
galaxy-s10 <2274751790@qq.com>
2020-11-26 18:32:43 +0800

    1126

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }));
 // parse application/json
 app.use(bodyParser.json())
 
-// app.all("*", function (req, res, next) {
-//   //设置允许跨域的域名，*代表允许任意域名跨域
-//   res.header("Access-Control-Allow-Origin", "*");
-//   //允许的header类型
-//   res.header("Access-Control-Allow-Headers", "content-type");
-//   //跨域允许的请求方式 
-//   res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
-//   if (req.method.toLowerCase() == 'options')
-//     res.send(200);  //让options尝试请求快速结束
-//   else
-//     next();
-// })
+app.all("*", function (req, res, next) {
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin", "*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers", "content-type");
+  //跨域允许的请求方式 
+  res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.send(200);  //让options尝试请求快速结束
+  else
+    next();
+})
 
 
 let relation = require("./models/relation")
