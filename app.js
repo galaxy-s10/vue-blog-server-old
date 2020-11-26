2a4296b157182424fa3532bcde823a3e63283d5f
galaxy-s10 <2274751790@qq.com>
2020-11-27 01:00:31 +0800

    配置允许跨域以及携带部分参数

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.all("*", function (req, res, next) {
   //设置允许跨域的域名，*代表允许任意域名跨域
   res.header("Access-Control-Allow-Origin", "*");
   //允许的header类型
-  res.header("Access-Control-Allow-Headers", "content-type");
+  res.header("Access-Control-Allow-Headers", "Content-Type,authorization,request-origin");
   //跨域允许的请求方式 
   res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
   if (req.method.toLowerCase() == 'options')
